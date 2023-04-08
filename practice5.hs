22a2600e6634b035684cf6b0ac31f5e217f6f5f3
tomyrodeghiero <tomyrodeghiero@gmail.com>
2023-04-07 21:09:11 -0300

    Update Practice 5

--- practice5.hs
+++ practice5.hs
@@ -39,8 +39,8 @@ infiniteListNaturals n = n : infiniteListNaturals (Succ n)
 
 infiniteListNaturals' :: Int -> [Int]
 infiniteListNaturals' n
-    | n < 0 = [-1]
-    | otherwise = [n..]
+    | n < 0 = error "The input value must be greater than or equal zero"
+    | otherwise = [n, n + 1..]
 
 -- Exercise 3
 {- 3. Generar una lista con los primeros n naturales. -}
@@ -55,26 +55,38 @@ first5ElementsOfInfiniteList = take 5 ([1..])
 -- Utilizando funciones de alto orden resolver:
 -- Exercise 5
 {- 5. Dada una lista de enteros, retornar sus cuadrados. -}
-{- squaresListInt :: [Int] -> [Int]
-squaresListInt xs = [x * x | x <- xs] -}
+squaresListInt :: [Int] -> [Int]
+squaresListInt xs = map (^2) xs
 
 -- Exercise 6
 {- 6. Dado un entero positivo, retornar sus divisores. -}
-{- divsN :: Int -> [Int]
-divsN n = [x | x <- [1..n], n `mod` x == 0] -}
+dividersZPositive :: Int -> [Int]
+dividersZPositive n = filter (\x -> n `mod` x == 0) [1..n]
 
 -- Exercise 7
 {- 7. Dada una lista de naturales, obtener la lista que contenga solo los núumeros primos
 de la lista original. -}
+primeNumbers :: [Int] -> [Int]
+primeNumbers xs = filter (\x -> isPrime x) xs
+
+-- helper function
+isPrime :: Int -> Bool
+isPrime n = length (filter (\x -> n `mod` x == 0) [1..n]) == 2
+
+-- Exercise 8
+{- 8. Dada una lista de naturales, retornar la suma de los cuadrados de la lista. -}
+sumSquaresList :: [Int] -> Int
+sumSquaresList xs = sum (map (^2) xs)
+
+-- Exercise 9
+{- 9. Dada una lista de naturales, retornar la lista con sus sucesores. -}
+successorsList :: [Int] -> [Int]
+successorsList xs = map (+1) xs
 
 -- Exercise 10
 {- 10. Dada una lista de enteros, sumar todos sus elementos. -}
-{- sum' :: [Int] -> Int
-sum' [] = 0
-sum' (x:xs) = x + sum' xs -}
-
 sum' :: (Num a) => [a] -> a
-sum' xs = foldl (\acc x -> acc + x) 0 xs
+sum' xs = foldr (+) 0 xs
 
 -- Exercise 11
 {- 11. Definir el factorial usando fold. -}
@@ -88,9 +100,32 @@ successors :: [Int] -> [Int]
 successors xs = [x + 1 | x <- xs]
 
 -- Exercise 13
+{- 13. Dada una lista de naturales, retornar sus cuadrados. -}
+squares :: [Int] -> [Int]
+squares xs = [x ^ 2 | x <- xs]
+
+-- Exercise 14
+{- 14. Dada una lista de enteros, retornar los elementos pares que sean mayores a 10. -}
+pairsGreater10 :: [Int] -> [Int]
+pairsGreater10 xs = [x | x <- xs, x `mod` 2 == 0, x > 10]
+
+-- Exercise 15
 {- 15. Dado un entero, retornar sus divisores. -}
 dividers :: Int -> [Int]
 dividers n = [x | x <- [1..n], n `mod` x == 0]
+
+-- Exercise 16
+{- 16. Dado un natural n, retornar los números primos comprendidos entre 2 y n. -}
+primesUntilN :: Int -> [Int]
+primesUntilN n = [x | x <- [2..n],isPrime x]
+    where
+        isPrime :: Int -> Bool
+        isPrime n = length [x | x <- [2..(n-1)], n `mod` x == 0] == 0
+
+-- Exercise 17
+{- 17. Dadas dos listas de naturales, retornar su producto cartesiano. -}
+cartesianProduct :: [Int] -> [Int] -> [(Int, Int)]
+cartesianProduct xs ys = [(x, y) | x <- xs, y <- ys]
 
 -- Exercise 18
 {- 18. Definir la lista infinita de los números pares. -}
